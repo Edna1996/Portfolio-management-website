c075756a8a4802c06256ce656cb9190c67e012b4
edna1996 <guantaiedna@gmapl.com>
2023-03-06 09:03:19 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import NavBar from './components/NavBar';
+import SkillsPage from './components/SkillsPage';
+import ProjectsPage from './components/ProjectsPage';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from "./components/NavBar";
-import { Skills } from "./components/Skills";
-import { Projects } from "./components/Projects";
-import { Footer } from "./components/Footer";
+import './components/home.css';
+import './components/navbar.css';
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <Skills />
-      <Projects />
-      <Footer />
-    </div>
-  );
+/* eslint-disable react/prefer-stateless-function */
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/skills" element={<SkillsPage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
